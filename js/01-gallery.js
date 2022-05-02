1b70e867e2dce5da9508828cbb372712a5878e18
VitaliyKhmil <hmilvitaliy@gmail.com>
2022-05-02 13:30:09 +0300

    02lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,37 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
 let instance;
 const galery = document.querySelector(".gallery");
 const items = galleryItems.map(item => {
-    // div
-    const div = document.createElement("div");
-    div.classList.add("gallery__item");
+    
+    const containerGallery = document.createElement("div");
+    containerGallery.classList.add("gallery__item");
 
-    // a
-    const a = document.createElement("a");
-    a.classList.add("gallery__link");
-    a.href = item.original;
+    const imgLink = document.createElement("a");
+    imgLink.classList.add("gallery__link");
+    imgLink.href = item.original;
 
-    // img
     const img = document.createElement("img");
     img.classList.add("gallery__image");
     img.src = item.preview;
-    img.setAttribute('data-source', item.original);
-    img.setAttribute('alt', item.description);
+    img.setAttribute("data-source", item.original);
+    img.setAttribute("alt", item.description);
 
-    a.appendChild(img);
-    div.appendChild(a);
+    imgLink.appendChild(img);
+    containerGallery.appendChild(imgLink);
 
-    return div;
+    return containerGallery;
 });
 
 galery.append(...items);
 
 function closeImage(event) {
     const visible = instance && instance.visible();
-    visible && event.keyCode === 27 && instance.close(() => {
-        //document.removeEventListener("keydown");
-    });
+    visible && event.keyCode === 27 && instance.close();
 }
 
 function selectImage(event) {
@@ -40,9 +36,7 @@ function selectImage(event) {
   }
     event.preventDefault();
     instance = basicLightbox.create(`<img src="${event.target.dataset.source}" />`);
-    instance.show(() => {
-        //document.addEventListener("keydown", closeImage);
-    });
+    instance.show();
 }
    
 galery.addEventListener("click", selectImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galery = document.querySelector(".gallery");
+const items = galleryItems.map(item => {
+        
+    const imgLink = document.createElement("a");
+    imgLink.classList.add("gallery__item");
+    imgLink.href = item.original;
+    
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = item.preview;
+    img.setAttribute("alt", item.description);
+
+    imgLink.appendChild(img);
+
+    return imgLink;
+});
+galery.append(...items);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    docClose: false,
+    captionsData: "alt",
+    captionDelay: 250,
+
+ });
